8925b9085a55982304aa90c86c9b2f3aace865a6
Juan  Segovia <1930194+juanlittledevil@users.noreply.github.com>
2024-03-09 23:08:24 +1300

    Ooops, how did that get in there? :)

--- src/UMIDI.cpp
+++ src/UMIDI.cpp
@@ -1,14 +1,4 @@
 #include "UMIDI.h"
-#include "Debug.h"
-// Uncomment the line below to enable debugging. Comment it out to disable debugging
-// each file has its own DEBUG flag for more granular control.
-#define DEBUG 1 // 0 for no debug, 1 for debug
-#ifdef DEBUG
-#define DEBUG_PRINT(message) Debug::print(__FILE__, __LINE__, __func__, String(message))
-
-// Include the Arduino Serial library
-#include <Arduino.h>
-#endif
 
 // Constructor with serial port initialization
 UMIDI::UMIDI(uint8_t rxPin, uint8_t txPin) : serialPort(rxPin, txPin) {
